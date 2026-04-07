2fc767634de9256ac3dc69859d7431b9a6eba9d8
STM-Project <51048656+STM-Project@users.noreply.github.com>
2026-04-07 13:49:54 +0200

    Update string_oper.h

--- Core/Inc/string_oper.h
+++ Core/Inc/string_oper.h
@@ -8,6 +8,8 @@
 #ifndef INC_STRING_OPER_H_
 #define INC_STRING_OPER_H_
 
+#include <stdint.h>
+
 typedef enum{
 	Sign_plusMinus,
 	Sign_minus,
@@ -22,7 +24,9 @@ typedef enum{
 	Zero = '0'
 }Int2Str_freeSign;
 
-char* Int2Str	  (int value, char freeSign, int maxDigits, int plusMinus);
-void  float2stri  (char *buffer, float value, unsigned int dec_digits);
+char* 		  Int2Str	   (int value, char freeSign, int maxDigits, int plusMinus);
+void  		  float2stri   (char *buffer, float value, unsigned int dec_digits);
+char* 		  HexToAscii   (uint8_t val,int mem);
+char* 		  Hex2ToAscii  (uint16_t val,int mem);
 
 #endif /* INC_STRING_OPER_H_ */
